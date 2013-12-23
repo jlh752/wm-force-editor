f5bb2e4e5559b830fa39b2826d75455e10d3d3ed
Josh Hummel <jlh752@uowmail.edu.au>
2013-12-23 20:24:48 +1100

    load and show units

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,5 +5,11 @@
 var wmForceEd = angular.module('wmForceEd', []);
 
 wmForceEd.controller('wmForceEdCtrl', function($scope, $http) {
-	
+	$http.get('data.json').success(function(data) {
+		$scope.units = data['units'];
+		$scope.formations = data['formations'];
+		$scope.skills = data['skills'];
+		$scope.types = data['types'];
+		$scope.subtypes = data['subtypes'];
+	});
 });
