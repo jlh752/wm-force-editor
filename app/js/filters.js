c3824d724a15cf9eef4681e35a77932a166c77a5
Josh Hummel <jlh752@uowmail.edu.au>
2013-12-23 20:54:00 +1100

    unit filters, show units in correct category

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,3 +1,25 @@
 'use strict';
 
 /* Filters */
+wmForceEd.filter('unitCategoryFilter', function() {
+	return function(input, type) {
+		var out = [];
+		for(var i in input){
+			if(input[i].unitClass == type){
+				out.push(input[i]);
+			}
+		}
+		return out;
+	};
+});
+wmForceEd.filter('subtypeFilter', function() {
+	return function(input, type){
+		var out = [];
+		if(type == '' || type == undefined) return input;
+		for(var i in input){
+			if(input[i].unitType1 == type || input[i].unitType2 == type)
+				out.push(input[i]);
+		}
+		return out;
+	};
+});
