ddcdf205c3ec3bf8f2d9ca6fb0e8e39c1b1e6c47
Josh Hummel <jlh752@uowmail.edu.au>
2013-12-26 09:45:36 +1100

    unit picker scrolls and resizes so it is actually usable

--- app/js/directives.js
+++ app/js/directives.js
@@ -4,9 +4,17 @@
 wmForceEd.directive('backImg', function(){
     return function(scope, element, attrs){
         var url = attrs.backImg;
+		if(url.charAt(url.length-1) == '/') url += "!.jpg";
         element.css({
             'background-image': 'url(' + url +')',
             'background-size' : 'cover'
         });
     };
+});
+wmForceEd.directive('setupDirective', function() {
+	return function(scope, element, attrs) {
+		//if(scope.$last){
+			makeDraggable(element);//setupSidebar(element);
+		//}
+	};
 });
---
+++ app/js/interaction.js
@@ -0,0 +1,24 @@
+function sticky_relocate() {
+	var window_top = $(window).scrollTop();
+	var div_top = $('#scroller-anchor').offset().top;
+	
+	if (window_top > div_top){
+		$('#scroller').addClass('sticky');
+		$("#unused").height($(window).height() - $("#unused").position().top);
+	}else{
+		$('#scroller').removeClass('sticky');
+		$("#unused").height($(window).height()+window_top - $("#unused").offset().top);
+	}
+}
+$(function() {
+	$(window).scroll(sticky_relocate);
+	$(window).resize(sticky_relocate);
+	$('#scroller').css("min-height", $(window).height()+$(window).scrollTop() - $("#unused").offset().top);
+	sticky_relocate();
+});
+
+function copyTooltip(element){
+	var el = document.getElementById('unit-' + element.getAttribute('mid'));
+	if(el != null)
+		document.getElementById('tooltip').innerHTML = el.innerHTML;
+}
